cea1dced89b9550daeccb35a40c7ae7cad5f63ba
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-10-19 11:45:29 +0800

    Updates

---
+++ assets/lazyLoad-C_uj92ch.js
@@ -0,0 +1,1 @@
+import{j as a,M as i,B as o,T as r}from"./index-C9qVL6oi.js";const n=()=>a.jsx(i,{children:a.jsx(o,{children:a.jsx(r,{variant:"h6",color:"initial",children:"lazy loading or code splitting"})})});export{n as default};
