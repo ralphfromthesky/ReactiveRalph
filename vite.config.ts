15caa50724cde0a5317a2e717a265bc85da2a94b
ralphfromthesky <ralphsantolorin53@gmail.com>
2024-10-19 11:36:42 +0800

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import postcss from 'postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ReactJSRalph/',
+  base: '/ReactJSRalph2/',
   plugins: [react()],
   css: {
     postcss: {
